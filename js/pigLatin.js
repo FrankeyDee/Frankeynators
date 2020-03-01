6db36c4b21647a5be428234afadfec7dd74425bf
FrankeyDee <frankeydarban@gmail.com>
2020-03-01 10:55:23 -0800

    pushing

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,4 +1,6 @@
 $(document).on("click", "#translateBtn", function(){
+    console.log("eat a dick")
     var userInput = $("#userInput").val();
     $("#translated").text(userInput);
 });
+console.log('hi')

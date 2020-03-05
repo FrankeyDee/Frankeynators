97f876d61a895b4978a726a8160c1c1514d44610
Frankey D <frankeydarban@gmail.com>
2020-03-04 20:19:09 -0800

    Update pigLatin.js

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,4 +1,14 @@
 $("#translateBtn").click(function(){
-    var userInput = $("#userInput").val();
-    $("#translated").text(userInput);
+    let userInput = $("#inputDefault").val();
+    //change input to have all lower case
+    const originalWord = (userInput.toLowerCase());
+    //identify 1st character
+    let firstChar = (originalWord.charAt(0));
+    console.log(firstChar);
+    //isVowel
+    var vowels = ["a","e","i","o","u"];
+    if (firstChar = vowels) {
+        console.log(firstChar,vowels)
+    $("#translated").text(originalWord);
+    }
 });

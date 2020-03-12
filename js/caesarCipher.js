95a2dc1719dbaf111d491487c0eeaed8d6db8904
FrankeyDee <frankeydarban@gmail.com>
2020-03-11 23:04:32 -0700

    encode letter by letter

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,33 +1,32 @@
 $(document).ready(function() {
-
+    const oldPhabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     // Encode//
     $("#encode").click(function() {
-        let keyNumber = $("#keyNumber").val();
-
-        //user's plaintext input
-        let plainInput = $("#input_plaintext").val();
-        let plainArray = plainInput.split(' ');
-
-        const oldPhabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-        for (let i = 0; i < plainArray.length; i++) {
-            let wordArray = plainArray[i].split('')
-            for (let j = 0; j < wordArray.length; j++) {
-                if 
-                let plainKey = oldPhabet.indexOf(wordArray[j]);
-                let newKey = (Number(keyNumber) + Number(plainKey));
-                // console.log(newKey)
-                if (newKey > 25) {
-                    let cipherKey = newKey - 25
-                    let newLetter = oldPhabet[cipherKey];
-
+        //user's selected Key = plainKey
+        
+        const plainInput = $("#input_plaintext").val();
+        const key = $("#keyNumber").val()
+        let plainArray = plainInput.split('');
+            
+        for (let j = 0; j < plainArray.length; j++) {
+            let originalKey = oldPhabet.indexOf(plainArray[j])
+            //console.log(plainKey)
+            let newKey = (Number(key) + Number(originalKey));
+            
+            if (newKey > 25) {
+                let cipherKey = newKey - 26
+                console.log(cipherKey)
+                let newLetter = oldPhabet[cipherKey]
+                
                 console.log(newLetter);
-                }
+            } else {
+                let newLetter = oldPhabet[newKey];
+                console.log(newLetter);
             }
-        
-    }})
+        }
+    })
 });
-        
+        //build string
         
 //     // Decode//
 //     $("#decode").click(function() {
